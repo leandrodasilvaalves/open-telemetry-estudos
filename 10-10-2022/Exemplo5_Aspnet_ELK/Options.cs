20559341dc7f9da53ce1c44a04b6b7250c9df5ff
Leandro Alves <leandro.silva.alves86@gmail.com>
2022-10-12 20:13:41 -0300

    Add melhorias no exemplo 5

--- 10-10-2022/Exemplo5_Aspnet_ELK/Options.cs
+++ 10-10-2022/Exemplo5_Aspnet_ELK/Options.cs
@@ -1,19 +1,37 @@
 using System.Text.Json;
-internal class Options 
+internal class Options
 {
-    public string UrlClient { get; set; }  
-    public string OtelUrl { get; set; }     
     private string _serviceName;
     public string ServiceName
     {
-        get => $"Lele.Servico.{_serviceName}"; 
-        set =>  _serviceName = value; 
+        get => $"Lele.Servico.{_serviceName}";
+        set => _serviceName = value;
     }
-    
+
     public string ServiceVersion { get; set; }
+
+    public string UrlClient { get; set; }
+    public string OtelUrl { get; set; }
+    public JaegerOpt Jaeger { get; set; }
+    public ZipkinOpt Zipkin { get; set; }
 
     public override string ToString()
     {
         return JsonSerializer.Serialize(this);
     }
+}
+
+internal class JaegerOpt
+{
+    public string Url { get; set; }
+    public int Port { get; set; }
+}
+
+internal class ZipkinOpt
+{
+    public string Url { get; set; }
+    public int Port { get; set; }
+
+    public Uri Uri => new Uri($"{Url}:{Port}/api/v2/spans");
+
 }
--- 10-10-2022/Exemplo5_Aspnet_ELK/Program.cs
+++ 10-10-2022/Exemplo5_Aspnet_ELK/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using OpenTelemetry;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -11,15 +12,22 @@ builder.Services.Configure<Options>(
 var options = new Options();
 builder.Configuration.GetSection("Options").Bind(options);
 
-builder.Services.AddOpenTelemetryTracing(b =>
+
+builder.Services.AddOpenTelemetryTracing(providerBuilder =>
 {
-    b.AddConsoleExporter()
+    providerBuilder.AddConsoleExporter()
     .AddSource(options.ServiceName)
     .SetResourceBuilder(ResourceBuilder.CreateDefault()
         .AddService(options.ServiceName, options.ServiceVersion))
     .AddHttpClientInstrumentation()
     .AddAspNetCoreInstrumentation()
     .AddElasticsearchClientInstrumentation()
+    .AddJaegerExporter(opt =>
+    {
+        opt.AgentHost = options.Jaeger.Url;
+        opt.AgentPort = options.Jaeger.Port;
+    })
+    .AddZipkinExporter(opt => opt.Endpoint = options.Zipkin.Uri)
     .AddOtlpExporter(opt =>
     {
         opt.Endpoint = new Uri(options.OtelUrl);
@@ -31,11 +39,20 @@ builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+var source = new ActivitySource(options.ServiceName);
+
 app.MapGet("/posts/{id}", async (IHttpClientFactory factory, int id) =>
 {
+    var activity = source.StartActivity($"GET /posts/{id}");
+
     var client = factory.CreateClient();
+    activity.SetTag("created_client", client.MaxResponseContentBufferSize);
+
+    activity.SetTag("before_request", $"post_id:{id}");
     var response = await client.GetFromJsonAsync<Post>($"{options.UrlClient}/posts/{id}");
     await Task.Delay(new Random().Next(100, 2000));
+    activity.SetTag("after_request", $"post_id:{id}");
+
     return response;
 });
 
