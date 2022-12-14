778884b3f84db956dea8ba9f79c010e97c77b13f
Leandro Alves <leandro.silva.alves86@gmail.com>
2022-12-14 18:08:13 -0300

    Dockerizar apis

--- segunda-etapa/Demo/services/Demo.Payment.Api/Program.cs
+++ segunda-etapa/Demo/services/Demo.Payment.Api/Program.cs
@@ -9,17 +9,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ segunda-etapa/Demo/services/restart-services.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker-compose stop payment
+docker-compose stop catalog
+docker-compose stop stock
+
+docker-compose rm -f payment
+docker-compose rm -f catalog
+docker-compose rm -f stock
+
+docker-compose up -d 
+docker-compose logs -f --tail=all payment catalog stock
